f2ceeee3fc134ad3b1f33337628c16593a7cc8dd
Sofia Hudson <sofia-sama@hotmail.com>
2023-05-04 17:38:54 +0200

    Add second function not jet checked

--- index.test.js
+++ index.test.js
@@ -1,14 +1,26 @@
-import checkIfHasAllVowels from "./index.js";
+import checkIfItsEven from "./index.js";
 
-describe("Given a chekIfHasAllVowel function", () => {
+describe("Given a checkIfHasAllVowel function", () => {
   describe("When it receives vowels", () => {
     test("Then it should return true", () => {
       const word = "Supercalifragilisticoespiralidoso";
 
-      const result = checkIfHasAllVowels(word);
+      const result = checkIfItsEven(word);
       const expectedBoolean = true;
 
-      expect(expectedBoolean).toBe(result);
+      expect(result).toBe(expectedBoolean);
+    });
+  });
+});
+
+describe("Given a checkIfItsEven function", () => {
+  describe("When it reveies even number", () => {
+    test("Then it should return true", () => {
+      const number = 10;
+
+      const result = checkIfItsEven(number);
+
+      expect(result).toBe(true);
     });
   });
 });
